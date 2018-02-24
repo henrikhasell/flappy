28451f5ada1828fc4d04a9c088992fbf31469bfc
Henrik Adam Hasell <henrikhasell@gmail.com>
2018-02-24 18:51:25 +0000

    Added audio.

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ gulp.task('compile', function() {
 
 gulp.task('copy', function() {
     gulp.src([
+        'node_modules/howler/dist/howler.min.js',
         'node_modules/matter-js/build/matter.min.js',
         'node_modules/pixi.js/dist/pixi.min.js'
     ]).pipe(gulp.dest('wwwroot'));
